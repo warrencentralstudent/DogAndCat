625ce44b58b48aec76d6cd412dd3cb71818b72de
warrencentralstudent <affogdav000@warren.k12.in.us>
2024-12-16 01:41:48 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+}
+
+class Dog {
+    String name;
+    String breed;
+    int age;
+    static String species = "Canine";
+}
+
+class Cat {
+    String name;
+    String breed;
+    int age;
+    static String species = "Feline";
+}
+
